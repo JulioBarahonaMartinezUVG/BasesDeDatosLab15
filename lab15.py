b3935febd8485682a7e9bf11c58f00176d1e88ea
JulioBarahonaMartinezUVG <50158692+JulioBarahonaMartinezUVG@users.noreply.github.com>
2019-05-25 23:13:26 -0600

    Add files via upload

--- lab15.py
+++ lab15.py
@@ -1,10 +1,14 @@
-import pymongo
+from pymongo import MongoClient
+import pprint
+from datetime import datetime
+client = MongoClient()
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["lab15"]
-mycol = mydb["producto"]
+db = client.lab15
+db2 = client.lab152
 
-print(mydb.)
+#pulled items from db
+myCart = {}
+
 mylist = [
     {"item":"lapiz","marca":"bolik","numero":2,"precio": 2},
     {"item":"carro","marca":"toyota","kilometraje":5000,"gasolina":"super","precio":120000},
@@ -13,7 +17,101 @@ mylist = [
     {"item":"mousepad","color":"negro","precio":25}
     ]
 
-x = mycol.insert_many(mylist)
+items = db.posts
+cartsDone = db2.carts
+#-----------------#
+# PRODUCT CATALOG #
+#-----------------#
 
-#print list of the _id values of the inserted documents:
-print(x.inserted_ids)
+#--------#
+# Create #
+#--------#
+post_id = items.insert_many(mylist)
+
+#------#
+# Read #
+#------#
+
+pprint.pprint(items.find_one({"marca": "sager"}))
+
+#----------------------#
+# INVENTORY MANAGEMENT #
+#----------------------#
+
+#--------------------------#
+# Agreagar a shopping cart #
+#--------------------------#
+#busca y agrega al carrito el id dek producto que se busca
+def shoppingCart(name):
+
+    #convierte el nombre por si acaso
+    item = str(name)
+
+    #lo busca en la db
+    product = items.find_one({"marca": item})
+
+    #lo muestra
+    #pprint.pprint(product)
+
+    #id del item en el dictionar
+    myCartItemID = product['_id']
+
+    #si ya esta en el carrito
+    if myCartItemID in myCart:
+        myCart[myCartItemID] = myCart[myCartItemID] + 1
+
+    #si no lo esta
+    else:
+        myCart[myCartItemID] = 1
+
+
+
+#Demostracion
+print('buscando un producto sager ')
+shoppingCart('sager')
+print('cuando no hay ninugno en el carro lo pone como 1' ,  myCart)
+shoppingCart('bolik')
+shoppingCart('toyota')
+shoppingCart('toyota')
+shoppingCart('toyota')
+
+print('buscando un producto sager ')
+print('cuando hay mas de uno le suma como 1' , myCart)
+print(myCart)
+
+#---------------------------#
+# Cambiar cantidad de items #
+#---------------------------#
+def cambiarenCarrito(id,num):
+    product = items.find_one({"marca": str(id)})
+    if not(product == None):
+        currentAmount = myCart[product['_id']]
+        newAmount = currentAmount + num
+        myCart[product["_id"]] = newAmount
+        print('Carrito actulizado')
+        print(myCart)
+
+    else:
+        print('Producto no encontrado o numero no valido')
+
+#agregando 5 sager al carrito
+print('agregando 5 sager al carrito')
+cambiarenCarrito('sager',5)
+
+#-----------#
+# Check out #
+#-----------#
+def checkOut():
+    print('Su carrito contiene:')
+
+    for i in myCart:
+        print('* ' , myCart[i] , ' unidad(es) del producto con nombre' , items.find_one({"_id": i})['item'])
+
+    checkOutDate = datetime.now()
+
+    print(str(checkOutDate))
+    cartDone = {"date": str(checkOutDate) , "content":myCart}
+    print(cartDone)
+
+checkOut()
+
